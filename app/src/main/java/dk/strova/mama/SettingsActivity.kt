48a9aa340a21bc7a9651e17dae4ded667adc4de9
Mathias Rav <m@git.strova.dk>
2018-04-15 22:27:08 +0200

    Rename pref

--- app/src/main/java/dk/strova/mama/SettingsActivity.kt
+++ app/src/main/java/dk/strova/mama/SettingsActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity
 
 class SettingsActivity: AppCompatActivity() {
     companion object {
-        const val KEY_ENDPOINT = "endpoint"
+        const val KEY_ENDPOINT = "dk.strova.mama.pref_endpoint"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
